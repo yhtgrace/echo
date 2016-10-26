d77d23abbb289cc82f44cedbd3797c3fe0cbc76c
tbepler <tbepler@gmail.com>
2016-10-26 14:58:54 -0400

    implementing ventilator filter as sql query. row_id groupby appears broken

--- src/vent_echo.sql
+++ src/vent_echo.sql
@@ -1,68 +1,35 @@
--- Create a flag indicating whether the patient was on 
--- a ventilator when echo was taken
+-- Create a flag indicating whether the patient was on -- a ventilator when echo was taken
 DROP MATERIALIZED VIEW IF EXISTS vent_echo CASCADE; 
 
 CREATE MATERIALIZED VIEW vent_echo AS  
 
 WITH vent_echo AS (
 
-    SELECT echo.row_id, echo.subject_id, echo.hadm_id, icu.icustay_id, echo.charttime
-    FROM noteevents echo
-    INNER JOIN icustays icu
-        ON icu.hadm_id = echo.hadm_id
-    WHERE echo.category = 'Echo'
+    SELECT echo.row_id, echo.charttime
+    --ventilator flags
+    , ((vent.starttime <= echo.charttime) AND (vent.endtime >= echo.charttime)) AS on_ventilator_during_echo
+    , True AS on_ventilator_anytime
+    FROM echo_icustay echo
+    INNER JOIN ventdurations vent 
+        ON echo.icustay_id = vent.icustay_id
+),
 
+vent_echo_unique AS (
+    
+    SELECT row_id
+    , bool_or(on_ventilator_during_echo) as on_ventilator_during_echo
+    , bool_or(on_ventilator_anytime) as on_ventilator_anytime
+    FROM vent_echo
+    GROUP BY row_id
 
-
-
-    SELECT ed.row_id, ed.hadm_id, ed.charttime
-
-    -- icustay 
-    , ie.icustay_id
-    , ROUND( (CAST(ie.outtime AS DATE) - CAST(ie.intime AS DATE)), 4) AS los_icu
-    , DENSE_RANK() OVER (PARTITION BY ie.hadm_id ORDER BY ie.intime) AS icustay_seq
-    , CASE
-        WHEN DENSE_RANK() OVER (PARTITION BY ie.hadm_id ORDER BY ie.intime) = 1 THEN 'Y'
-        ELSE 'N' END AS first_icu_stay
-    , ie.outtime -- icu outtime 
-
-    -- patient 
-    , pat.gender
-    , pat.subject_id
-    , pat.dod
-
-    -- hospital 
-    , adm.admittime
-    , adm.dischtime -- hospital discharge time
-    , adm.ethnicity
-    , ROUND( (CAST(adm.admittime AS DATE) - CAST(pat.dob AS DATE))  / 365.242, 4) AS age
-    , adm.diagnosis
-
-
-    -- echos
-    , ed.charttime - adm.admittime AS time_to_echo
-
-    FROM icustays ie
-    INNER JOIN echodata ed
-        ON ie.hadm_id = ed.hadm_id
-    INNER JOIN admissions adm
-        ON ie.hadm_id = adm.hadm_id
-    INNER JOIN patients pat
-        ON ie.subject_id = pat.subject_id
-), 
-        
-max_icustay_seq AS (
-
-    SELECT eis.hadm_id
-    , MAX(eis.icustay_seq) AS max_icustay_seq
-    FROM echo_icustay as eis
-    GROUP BY eis.hadm_id
 )
 
-SELECT eis.*, mis.max_icustay_seq
-    , (( eis.time_to_echo > INTERVAL '-8 hours') AND 
-        (eis.time_to_echo < INTERVAL '48 hours')) AS time_filter 
-FROM echo_icustay eis
-INNER JOIN max_icustay_seq mis
-    ON eis.hadm_id = mis.hadm_id
-WHERE max_icustay_seq = 1
+SELECT echo.row_id
+, COALESCE(vent.on_ventilator_during_echo, False) as on_ventilator_during_echo
+, COALESCE(vent.on_ventilator_anytime, False) as on_ventilator_anytime
+FROM echo_icustay echo
+LEFT JOIN vent_echo_unique vent
+    ON echo.row_id = vent.row_id
+
+
+

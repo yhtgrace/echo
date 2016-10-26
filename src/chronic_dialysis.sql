eb113a4c56e992130bc6d2316956ada4b0ec6c2b
Minnan Xu <Minnan.Xu@philips.com>
2016-10-26 17:59:19 -0400

    code for dialysis -- for discussion with team

---
+++ src/chronic_dialysis.sql
@@ -0,0 +1,15 @@
+﻿SELECT subject_id, hadm_id, seq_num, icd9_code
+  FROM mimiciii.diagnoses_icd
+  where icd9_code in (
+	 '5855' -- Chron kidney dis stage V
+	,'5856' -- End stage renal disease
+	, '40301' -- "Mal hyp kid w cr kid V"
+	, '40311' -- "Ben hyp kid w cr kid V"
+	, '40391' -- "Hyp kid NOS w cr kid V"
+	, '40402' -- Mal hy ht/kd st V w/o hf"
+	, '40403' -- Mal hyp ht/kd stg V w hf"
+	, '40412' -- Ben hy ht/kd st V w/o hf"
+	, '40413' -- Ben hyp ht/kd stg V w hf"
+	, '40492' -- Hy ht/kd NOS st V w/o hf"
+	, '40493' -- Hyp ht/kd NOS st V w hf
+	)

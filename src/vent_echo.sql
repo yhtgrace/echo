7be81dc06cfdd2ce5ddca089ada3966e6f6fe355
tbepler <tbepler@gmail.com>
2016-10-26 16:28:21 -0400

    added more echo filters with ventilator filter

--- src/vent_echo.sql
+++ src/vent_echo.sql
@@ -9,7 +9,7 @@ WITH vent_echo AS (
     --ventilator flags
     , ((vent.starttime <= echo.charttime) AND (vent.endtime >= echo.charttime)) AS on_ventilator_during_echo
     , True AS on_ventilator_anytime
-    FROM echo_icustay echo
+    FROM echodata echo
     INNER JOIN ventdurations vent 
         ON echo.icustay_id = vent.icustay_id
 ),
@@ -27,7 +27,7 @@ vent_echo_unique AS (
 SELECT echo.row_id
 , COALESCE(vent.on_ventilator_during_echo, False) as on_ventilator_during_echo
 , COALESCE(vent.on_ventilator_anytime, False) as on_ventilator_anytime
-FROM echo_icustay echo
+FROM echodata echo
 LEFT JOIN vent_echo_unique vent
     ON echo.row_id = vent.row_id
 

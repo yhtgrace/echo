69821424416738aab50dc8654f1ff376a1a73637
yhtgrace <yhtgrace@gmail.com>
2016-10-31 18:12:58 -0400

    Set up the time filter in echo_icustay, and recompute counts for the dictionary tables with the time filter on.

--- src/copy.sql
+++ src/copy.sql
@@ -1,3 +1,3 @@
 -- write a table in csv format to stdout
---COPY (SELECT * FROM d_labs_all) TO STDOUT WITH CSV HEADER
---COPY (SELECT DISTINCT category FROM d_items) TO STDOUT WITH CSV HEADER
+--COPY (SELECT * FROM d_diagnoses_xc) TO STDOUT WITH CSV HEADER
+COPY (SELECT DISTINCT route FROM prescriptions) TO STDOUT WITH CSV HEADER
--- src/echo_icustay.sql
+++ src/echo_icustay.sql
@@ -6,10 +6,23 @@ CREATE MATERIALIZED VIEW echo_icustay AS
 -- by associating echos to hadm_ids
 -- and finding the icustay_id where the echo charttime is between (intime - 8h, outtime)
 -- this also filters for echos to which an icustay_id can be assigned
-SELECT 
-    ed.row_id, ed.charttime 
-    ,ie.icustay_id, ie.intime, ie.outtime, ie.hadm_id
-FROM echodata ed
-INNER JOIN icustays ie
-    ON ie.hadm_id = ed.hadm_id
-    WHERE (ed.charttime > (ie.intime - INTERVAL '8 hours')) AND (ed.charttime < ie.outtime)
+WITH echo_assigned AS ( 
+    SELECT 
+        ed.row_id, ed.charttime 
+        ,ie.icustay_id, ie.intime, ie.outtime, ie.hadm_id
+        ,(ed.charttime - ie.intime) AS time_to_echo
+    FROM echodata ed
+    INNER JOIN icustays ie
+        ON ie.hadm_id = ed.hadm_id
+        WHERE (ed.charttime > (ie.intime - INTERVAL '8 hours')) AND (ed.charttime < ie.outtime)
+)
+-- the current proposed sequence of filters starts with filtering for the echo in 
+-- the first 48 hours of ICU stay, and then filtering for echos which can be associated
+-- with distributive shock
+, add_filters AS (
+    SELECT *
+        , ((ea.time_to_echo > INTERVAL '-8 hours') AND 
+           (ea.time_to_echo < INTERVAL '48 hours')) AS time_filter
+    FROM echo_assigned ea
+)
+SELECT * FROM add_filters

534239746e8e7f0a57c1dbed9ecf3f39be53a723
yhtgrace <yhtgrace@gmail.com>
2016-12-03 18:08:53 -0500

    Compile variables for masterlist (Demographics, Filter variables, Labs)

--- src/echo_features.sql
+++ src/echo_features.sql
@@ -22,8 +22,8 @@ SELECT ef.row_id, ef.icustay_id, ef.hadm_id, ef.subject_id
    
     -- outcomes
     ,pt.dod -- date of death
-    ,pt.dod > ic.outtime AS survived_icustay-- survived icustay
-    ,pt.dod > am.dischtime AS survived_hadm-- survived hospital admission
+    ,pt.dod > ic.outtime AS survived_icustay -- survived icustay
+    ,pt.dod > am.dischtime AS survived_hadm -- survived hospital admission
 
     -- echo info
     ,ed.chartdate AS echo_chartdate
@@ -35,6 +35,32 @@ SELECT ef.row_id, ef.icustay_id, ef.hadm_id, ef.subject_id
     ,ed.bpdias AS echo_bpdias 
     ,ed.hr AS echo_hr
 
+    -- labs
+    ,ls.lab_albumin_min, ls.lab_albumin_max
+    ,ls.lab_bicarbonate_min, ls.lab_bicarbonate_max
+    ,ls.lab_ckmb_min, ls.lab_ckmb_max
+    ,ls.lab_creatinine_min, ls.lab_creatinine_max
+    ,ls.lab_crp_min, ls.lab_crp_max
+    ,ls.lab_egfr_min, ls.lab_egfr_max
+    ,ls.lab_hematocrit_min, ls.lab_hematocrit_max
+    ,ls.lab_inr_min, ls.lab_inr_max
+    ,ls.lab_lactate_min, ls.lab_lactate_max
+    ,ls.lab_platelet_min, ls.lab_platelet_max
+    ,ls.lab_ntprobnp_min, ls.lab_ntprobnp_max
+    ,ls.lab_ph_min, ls.lab_ph_max
+    ,ls.lab_tropi_min, ls.lab_tropi_max
+    ,ls.lab_tropt_min, ls.lab_tropt_max
+    ,ls.lab_wbc_min, ls.lab_wbc_max
+
+    -- filters
+    ,ef.ps_vaso -- if patient was on vasopressor during the icustay
+    ,ef.diag_xc -- if patient has an excluded diagnosis during the hospital admission
+    ,ef.age_filter -- age filter
+    ,ef.intime_to_echo -- icustay intime to echo
+    ,ef.time_filter -- (icustay intime to echo) between -8 to 48 hours
+    ,ef.after_rowid -- row_id of most proximal outpatient echo after this echo
+    ,ef.before_rowid -- row_id of most proximal outpatient echo before this echo
+
 FROM echo_filter_vars ef
 INNER JOIN icustays ic
     ON ef.icustay_id = ic.icustay_id
@@ -44,3 +70,5 @@ INNER JOIN admissions am
     ON ef.hadm_id = am.hadm_id
 INNER JOIN echodata ed
     ON ef.row_id = ed.row_id
+INNER JOIN echo_features_labs ls
+    ON ef.row_id = ls.row_id

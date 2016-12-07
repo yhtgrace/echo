93143e5bdf287c11f390ef0675cf9e89364d1831
Minnan Xu <Minnan.Xu@philips.com>
2016-12-06 21:57:59 -0500

    shifted  fluid_balance day 1,2, 3 to days 0,1,2

--- src/echo_filtered_mx.sql
+++ src/echo_filtered_mx.sql
@@ -1,12 +1,11 @@
-﻿-- for a clean copy of echo_filtered, run in order
+﻿set search_path to mimiciii;
+-- for a clean copy of echo_filtered, run in order
 -- echo_icustay (25512)
 -- diagnoses_xc_annot (392)
 -- d_prescriptions_vaso (49)
 -- echo_outpatient (4385)
 -- echo_filter_vars (25512)
--- echo_filter_vars_mx (25512)
--- combine_echo_filter_vars (25512)
--- echo_filtered (1863) 
+-- echo_features_mx (25512)
 
 DROP MATERIALIZED VIEW IF EXISTS echo_filtered_mx CASCADE;
 
@@ -15,8 +14,8 @@ CREATE MATERIALIZED VIEW echo_filtered_mx AS
 WITH 
   passes_filters AS (
     SELECT *
-        , DENSE_RANK() OVER (PARTITION BY ef.subject_id ORDER BY ef.charttime) AS echo_seq 
-    FROM combine_echo_filter_vars ef
+        , DENSE_RANK() OVER (PARTITION BY ef.subject_id ORDER BY ef.ed_charttime) AS echo_seq 
+    FROM echo_features_mx ef
     WHERE (
             ef.ps_vaso = True 
         AND ef.diag_xc = False
--- src/test_echofilter_echoannot2.sql
+++ src/test_echofilter_echoannot2.sql
@@ -1,4 +1,5 @@
-﻿---- Run these first: --
+﻿set search_path to mimiciii;
+---- Run these first: --
 -- echo_filter_vars_mx
 -- echo_filtered_mx
 
@@ -6,8 +7,10 @@ DROP MATERIALIZED VIEW IF EXISTS test_echo_fluid2 CASCADE;
 
 CREATE MATERIALIZED VIEW test_echo_fluid2 AS
 
-SELECT ef.*, (fd.chartdate - cast(ef.charttime as date)) as day_wrt_echo, fd.daily_input_ml, fd.daily_output_ml, fd.daily_balance_ml -- ef.row_id, ef.icustay_id
+SELECT ef.*, (fd.chartdate - cast(ef.ed_charttime as date)) as day_wrt_echo, fd.daily_input_ml, fd.daily_output_ml, fd.daily_balance_ml -- ef.row_id, ef.icustay_id
     FROM mimiciii.echo_filtered_mx as ef
     INNER JOIN mimiciii.fluid_dailybalance fd
         ON ef.icustay_id = fd.icustay_id
-order by fd.subject_id, ef.charttime, day_wrt_echo 
+    --INNER JOIN mimiciii.echo_annotations_unique ea
+    --    ON ef.icustay_id = ea.icustay_id 
+--order by fd.subject_id, ef.ed_charttime, day_wrt_echo 

341af7796ff7baf69c1a4b1efdc28609a947fadf
Minnan Xu <Minnan.Xu@philips.com>
2017-02-06 14:45:21 -0500

    updated/corrected fluid_balance_wrt_icuadmission.sql

--- src/fluid_balance_wrt_icuadmission.sql
+++ src/fluid_balance_wrt_icuadmission.sql
@@ -4,9 +4,9 @@
 -- fluid_cv_dailytotal, fluid_mv_dailytotal, output_dialytotal
 -- fluid_dailybalance
 
-DROP MATERIALIZED VIEW IF EXISTS icu_fluid_balance CASCADE;
+DROP MATERIALIZED VIEW IF EXISTS fluid_dailybalance_wrt_icuadmission CASCADE;
 
-CREATE MATERIALIZED VIEW icu_fluid_balance AS
+CREATE MATERIALIZED VIEW fluid_dailybalance_wrt_icuadmission AS
 
 SELECT ic.row_id, ic.subject_id, ic.icustay_id, ic.hadm_id, ic.intime, fd.chartdate, (fd.chartdate - cast(ic.intime as date)) as day_wrt_icuadmit, fd.daily_input_ml, fd.daily_output_ml, fd.daily_balance_ml
     FROM mimiciii.icustays as ic

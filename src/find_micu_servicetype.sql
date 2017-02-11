ac0153ac07f5dd2c19dbb2d14ed4e0e13c3b9858
Minnan Xu <Minnan.Xu@philips.com>
2017-02-11 06:39:01 -0500

    extract service type (MICU, SICU, NSICU) from chartevent table. (not all icustayids seem to have service type charted though).

---
+++ src/find_micu_servicetype.sql
@@ -0,0 +1,27 @@
+﻿set search_path to mimiciii;
+
+DROP MATERIALIZED VIEW IF EXISTS service_type_MICU_SICU_NSICU CASCADE;
+
+CREATE MATERIALIZED VIEW service_type_MICU_SICU_NSICU AS
+
+with service_type as (
+SELECT subject_id, hadm_id, icustay_id, value
+  FROM mimiciii.chartevents
+  where itemid in (
+  '1125'--"Service Type"  carevue
+  --'919' --"Service" carevue
+  --,'225518'-- "Services (Bronch)" metavision
+  --,'225414'--"Home without services", metavision
+  --,'225415'--"Home with services" - metavision. 
+  ,'224640'--"Service" --metavision
+) --and lower(value) in ('micu', 'm', 'md', 'med', 'medicine')  
+group by subject_id, hadm_id, icustay_id, value
+order by subject_id, hadm_id, icustay_id
+
+)
+
+select *,  
+  CASE WHEN value in ('MICU', 'MSICU', 'MICU/SICU') then 1 else 0 END AS MICU,
+  CASE WHEN value in ('SICU', 'MSICU', 'T-SICU', 'MICU/SICU') then 1 else 0 END AS SICU,
+  CASE WHEN value in ('NSICU') then 1 else 0 END AS NSICU
+from service_type
